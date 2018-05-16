125a758c2645c3a3451743126c040923813782f9
gamerlab <357681911qq@gmail.com>
2018-05-16 15:35:09 +0800

    add scowview on the middle area,move tab on bottom button clicked

---
+++ src/client/gta/Assets/Scripts/UI/MoveScrollViewContent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveScrollViewContent : MonoBehaviour {
+
+    public enum TABNUMBER {ONE=1,TWO=2,THREE=3,FOUR=4 }
+    public TABNUMBER tabNumer = TABNUMBER.ONE;
+    public int tabWidth = 1080;
+
+    public RectTransform content;
+    private Vector2 contentInitSizeDelta;
+    private void OnEnable()
+    {
+        UIEventHandle.EBottomButtonClick += MoveTab;
+    }
+
+    private void OnDisable()
+    {
+        UIEventHandle.EBottomButtonClick -= MoveTab;
+    }
+
+    // Use this for initialization
+    void Start () {
+        content = GetComponent<RectTransform>();
+        if (checkRectTransfromComponent()) {
+            contentInitSizeDelta = content.sizeDelta;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void MoveTab(TABNUMBER _tabNumber) {
+        if (checkRectTransfromComponent()) {
+            Debug.Log("offsetMin="+content.offsetMin);
+            Debug.Log("offsetMax="+content.offsetMax);
+            Debug.Log("sizeDelta"+content.sizeDelta);
+            content.offsetMin = new Vector2(tabWidth*(((int)_tabNumber)-1)*-1.0f, content.offsetMin.y);
+            content.sizeDelta = contentInitSizeDelta;
+        }
+    }
+    private bool checkRectTransfromComponent()
+    {
+        if (content == null)
+        {
+            Debug.Log("required RectTransform component on gameobject that name =" + gameObject.name);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/client/gta/Assets/Scripts/UI/UIEventHandle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEventHandle : MonoBehaviour {
+    public delegate void  DBottomButtonClick(MoveScrollViewContent.TABNUMBER tabNumber);
+    public static event DBottomButtonClick EBottomButtonClick;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //click events
+    public void OnMessageButtonClick()
+    {
+        Debug.Log("bottom button message click");
+        EBottomButtonClick(MoveScrollViewContent.TABNUMBER.ONE);
+    }
+    public void OnProjectButtonClick()
+    {
+        Debug.Log("bottom button project click");
+        EBottomButtonClick(MoveScrollViewContent.TABNUMBER.TWO);
+    }
+    public void OnMacketButtonClick()
+    {
+        Debug.Log("bottom button macket click");
+        EBottomButtonClick(MoveScrollViewContent.TABNUMBER.THREE);
+    }
+    public void OnMyButtonClick()
+    {
+        Debug.Log("bottom button my click");
+        EBottomButtonClick(MoveScrollViewContent.TABNUMBER.FOUR);
+    }
+}
